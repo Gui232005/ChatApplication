8c161a0f8cfa8cc267ba221b211d5ea306aa619a
Guilherme Silva <162608929+Gui232005@users.noreply.github.com>
2025-11-27 22:20:31 +0000

    Add UserAPI class with base URL
    Introduces the UserAPI class in user.dart, setting up the base URL for user-related API requests. This prepares the file for future user API methods.

---
+++ chat_application/lib/API/user.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class UserAPI {
+  static const String baseUrl = 'https://chat-application-noie.onrender.com/users/';
+
+  UserAPI();
+}
